76245bf5c04ec2a71e10409c6948b658d803fc5e
Developer Joy <joysarker2927@gmail.com>
2023-06-19 01:55:33 +0600

    data modeling done

--- api/models/userModels.js
+++ api/models/userModels.js
@@ -5,48 +5,125 @@ import mongoose  from "mongoose";
 // Creating student schma
 const usersSchma = mongoose.Schema({
 
-    name : {
+    first_name : {
         type : String,
         require : true,
+        trim : true
+    },
+    sur_name : {
+        type : String,
+        require : true,
+        trim : true
+    },
+    secondary_name : {
+        type : String,
+        require : true,
+        trim : true
+    },
+    username : {
+        type : String,
         trim : true
     },
     email : {
         type : String,
-        require : true,
         unique : true,
         trim : true
     },
-    call : {
+    mobile : {
         type : String,
-        require : true,
+        unique : true,
         trim : true
     },
-    age : {
-        type : Number,
-        require : true,
+    birth_date : {
+        type : String,
     },
     gender : {
-        type : String
-    },
-    username : {
         type : String,
-        require : true,
-        unique : true,
-        trim : true
+        enum : ["Male","Female","Custom"]
     },
     password : {
         type : String,
         require : true,
         trim : true
     },
-    photo : {
+    profile_photo : {
         type : String,
+        default : null
     },
-    status : {
-        type : Boolean,
+    cover_photo : {
+        type : String,
+        default : ""
+    },
+    bio : {
+        type : String,
+        default : null
+    },
+    work : {
+        type : Array,
+        default : []
+    },
+    edu : {
+        type : Array,
+        default : []
+    },
+    living : {
+        type : String,
+        
+    },
+    home_town : {
+        type : String,
+        
+    },
+    relationship : {
+        type : String,
+        enum : ["Single","Married","In a Relationship"]
+        
+    },
+    joined : {
+        type : Date
+        
+    },
+    social : {
+        type : Array,
+        default : []        
+    },
+    fridens : {
+        type : Array,
+        default : []        
+    },
+    following : {
+        type : Array,
+        default : []        
+    },
+    following : {
+        type : Array,
+        default : []        
+    },
+    follower : {
+        type : Array,
+        default : true
+    },
+    request : {
+        type : Array,
+        default : true
+    },
+    block : {
+        type : Array,
+        default : true
+    },
+    posts : {
+        type : Array,
         default : true
     },
     trash : {
+        type : Boolean,
+        default : false
+    },
+    access_token: {
+        type : Boolean,
+        default : false
+    },
+    isActivate : {
         type : Boolean,
         default : false
     },
